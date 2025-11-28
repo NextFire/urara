676afa1cbc7033847b0cc302f0a45f4ac2105637
NextFire <git@yuru.moe>
2025-11-28 19:28:06 +0100

    more mcp servers

--- urara/agent.py
+++ urara/agent.py
@@ -1,13 +1,19 @@
 import os
 
 from pydantic import constr
-from pydantic_ai import Agent
+from pydantic_ai import Agent, FunctionToolset
+from pydantic_ai.common_tools.tavily import tavily_search_tool
 from pydantic_ai.mcp import MCPServerStdio, MCPServerStreamableHTTP
 
+OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL", "openai/gpt-4.1-mini")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+assert TAVILY_API_KEY
+
 agent = Agent(
-    f"openrouter:{os.getenv('OPENROUTER_MODEL', 'openai/gpt-4.1-mini')}",
+    f"openrouter:{OPENROUTER_MODEL}",
     output_type=constr(max_length=2000),
     toolsets=[
+        FunctionToolset([tavily_search_tool(TAVILY_API_KEY)]),
         MCPServerStreamableHTTP(
             "https://mcp.deepwiki.com/mcp",
             timeout=60,
